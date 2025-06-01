7caf9db8b916f2ea38617b38ea8598a63f6f9933
gummy-cmd <gharma.peniyamina@my.jcu.edu.au>
2025-06-02 09:07:57 +1000

    Refactor passward_stars to use functions

--- prac_02/passward_stars.py
+++ prac_02/passward_stars.py
@@ -1,12 +1,26 @@
 """
 User Password Check
 """
-password_length = 6
-password = input("Enter Password: ")
-while len(password) < password_length:
-    print("Password too short")
+def main():
+
+    password_length = 6
+    password = get_password(password_length)
+    print_asterisks(password)
+
+
+def print_asterisks(password):
+    print("*" * len(password))
+
+
+def get_password(password_length):
     password = input("Enter Password: ")
-print("*" * len(password))
+    while len(password) < password_length:
+        print("Password too short")
+        password = input("Enter Password: ")
+    return password
+
+
+main()
 
 
 
