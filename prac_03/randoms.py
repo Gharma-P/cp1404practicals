cfb1f0c775c7f336f412b035d9f433419c7735c4
gummy-cmd <gharma.peniyamina@my.jcu.edu.au>
2025-06-07 12:53:14 +1000

    print random

---
+++ prac_03/randoms.py
@@ -0,0 +1,17 @@
+"""
+Random
+"""
+import random
+
+print(random.randint(0, 100))
+
+#What did you see on line 1?
+    #Printed Integer, largest = 20, smallest = 5
+
+#What did you see on line 2?
+    #Smallest = 3, Largest = 9
+    #Could not have produced a 4 because it goes up by 2 starting from 3
+
+#What did you see on line 3?
+    #Smallest = 2.69(rounded), Largest = 5.43 (rounded)
+
