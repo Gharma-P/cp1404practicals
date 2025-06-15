f60e842e0909cd19b55080fe6c6fcb7ab302570d
gummy-cmd <gharma.peniyamina@my.jcu.edu.au>
2025-06-15 22:35:18 +1000

    Getting random quick picks

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -2,9 +2,11 @@
 Write a program asking users how many quick picks they wish to generate
 """
 import random
+
 MAX = 45
 MIN = 1
 NUMBER = 6
+
 
 def main():
     get_picks = int(input("How many picks would you like to generate? "))
@@ -12,6 +14,7 @@ def main():
         picks = sort_pick()
         line = " ".join(f"{n:2}" for n in picks)
         print(line)
+
 
 def sort_pick():
     picks = []
@@ -21,7 +24,6 @@ def sort_pick():
             picks.append(random_number)
     picks.sort()
     return picks
-
 
 
 main()
