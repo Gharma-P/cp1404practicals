7c3f105719874cb32f393d5c377f4c83e443dbb7
gummy-cmd <gharma.peniyamina@my.jcu.edu.au>
2025-06-19 20:18:08 +1000

    get hexadecimal colour

---
+++ prac_05/hex_colours.py
@@ -0,0 +1,17 @@
+"""
+Create a program that allows you to look up hexadecimal colour codes
+"""
+
+COLOUR_TO_HEX = {"absolutezero": "#0048ba", "acidgreen": "#b0bf1a", "aliceblue": "#f0f8ff",
+                 "alizarincrimson": "#e32636", "amaranth": "#e52b50", "amber": "#ffbf00", "amethyst": "#9966cc",
+                 "antiqueahite": "#faebd7", "apricot": "#fbceb1", "aqua": "#00ffff"}
+
+get_hex = input("Enter a colour name: ").lower()
+
+while get_hex != "":
+    try:
+        print(f"{get_hex} is {COLOUR_TO_HEX[get_hex]}")
+    except KeyError:
+        print("Invalid state")
+    get_hex = input("Enter a colour name: ").lower()
+
