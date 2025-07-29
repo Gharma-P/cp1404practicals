1e91c58d443f7a6c45e7a7204f9282dd64238c04
gummy-cmd <gharma.peniyamina@my.jcu.edu.au>
2025-07-29 10:35:39 +1000

    Removed an extra button, that wasnt supposed to be there

--- prac_08/box_layout_demo.py
+++ prac_08/box_layout_demo.py
@@ -14,7 +14,7 @@ class BoxLayoutDemo(App):
 
     def clear(self):
         self.root.ids.output_label.text = ""
-        self.root.ids.input_name.text = ""
+        self.root.ids.input_name.text = ''
         return self.root
 
 
